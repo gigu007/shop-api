839add3c63fdca0aaa957083bd10e66356b60a1a
gigu007 <gideonukaegbu007@gmail.com>
2022-04-06 16:29:13 +0100

    another commit

--- controllers/products.js
+++ controllers/products.js
@@ -7,16 +7,18 @@ const getAllProductsStatic = async (req,res)=>{
    
     // throw new Error('testing async errors')
     
-        const search ='g'//any name that contains letter a
+        // const search ='g'//any name that contains letter a
         const products = await Product.find({
-        name:{$regex:search, $options:'i'}, //i means case sensitive
+        // name:{$regex:search, $options:'i'}, //i means case insensitive
+        
 
         
-    })
+    }).select('name price').limit(4)// selecting a some fields only
+    // .sort('-name -price')//this will sort from z-a while price will be from max - min
     res.status(200).json({products,nbHits:products.length})
 }
 const getAllProducts = async(req,res)=>{
-    const {featured,company,name,search} = req.query
+    const {featured,company,name,sort,fields} = req.query//the query string
     // console.log(re.query)
     const queryObject={}
     if(featured){
@@ -28,8 +30,26 @@ const getAllProducts = async(req,res)=>{
     if(name){
         queryObject.name={$regex: name, $options: 'i'}
     }
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+    // console.log(queryObject)
+    let result =  Product.find(queryObject)
+    //sort
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+       result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createdAt')//sorting based on time created
+    }
+    if(fields){
+        const fieldList = fields.split(',').join(' ')
+       result = result.select(fieldList)
+       //.limit(3)//this will return maximum of 3 results
+       //.skip(1)will skip the first item before return the rest
+    }
+    const page = Number(req.query.page) || 1
+    const limit =Number(req.query.limit)|| 10
+    const skip = (page -1)*limit
+    const products = await result;
     res.status(200).json({products,nbHits:products.length})
     // res.status(200).json({msg:`products  route`})
 }
